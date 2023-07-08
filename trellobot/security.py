7e62358d8e0434ee7584bc50b67d39ffb1680822
Devs Gryphsoft <devs@gryphsoft.com>
2023-07-08 18:51:30 -0300

    the pm param is obsolete and unused

--- trellobot/security.py
+++ trellobot/security.py
@@ -8,12 +8,12 @@ import logging
 authorized_user = None
 
 
-def security_check(bot, update, pm='md'):
+def security_check(bot, update):
     """Return a list with one or zero Messenger depending on auth result."""
     if update.message.chat_id == authorized_user:
         logging.info('Requested security check authorized')
-        return [Messenger(bot, update, parse_mode=pm)]
+        return [Messenger(bot, update)]
     else:
         logging.info('Requested security checkL authorized')
-        Messenger(bot, update, 'You are not authorized.', parse_mode=pm)
+        Messenger(bot, update, 'You are not authorized.')
         return []
